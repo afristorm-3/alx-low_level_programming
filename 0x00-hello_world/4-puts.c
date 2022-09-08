720ee8e685ed5aaff9d7296fc700575f18378a55
afristorm-3 <afristorm3@gmail.com>
2022-09-08 11:46:38 -0400

    print put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+*main -Entry Point
+*Return: Always 0(Success)
+*
+*/
 int main(void)
 {
-  puts ("Programming is like building a multilingual Puzzle\n");
-  return(0);
-  
-  }
+         puts ("Programming is like building a multilingual Puzzle\n");
+         return(0);
+}
