0af13b33132b84355c2a64141044912da25a6d18
afristorm-3 <afristorm3@gmail.com>
2022-09-09 12:54:25 -0400

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the lowercase alphabet in reverse.
+ * You can only use the putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+int num = 122;
+
+while (num > 96)
+{
+putchar(num);
+num -= 1;
+}
+putchar('\n');
+return (0);
+}
