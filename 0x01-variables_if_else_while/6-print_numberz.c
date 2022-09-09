eb91c4088bfc1ce3f46d07ce1c16c9f6485d2409
afristorm-3 <afristorm3@gmail.com>
2022-09-09 12:49:14 -0400

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	int main(void)
+	{
+		int num = 48;
+
+		while (num <= 57)
+
+	{
+		putchar(num);
+		num++;
+	}
+		putchar('\n');
+		return (0);
+	}
