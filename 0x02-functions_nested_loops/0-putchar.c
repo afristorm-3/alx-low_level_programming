70c24acbd7b98a83452821161d026a764b17a9ce
afristorm-3 <afristorm3@gmail.com>
2022-09-13 18:01:03 -0400

    main hedge

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - main program file
+ * 
+ * show how to use header file
+ * Return 0 to indicate success
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c);
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
