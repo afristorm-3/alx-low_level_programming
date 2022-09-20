6b93f4185eb8776996eae364abcf4a52b243bff4
afristorm-3 <afristorm3@gmail.com>
2022-09-20 07:22:28 -0400

    update the value ofvariable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of  variable
+ * the pointer points to to 98
+ *@n: pointer to the variable to update
+ * Return always 0.
+ */
+
+void resent_to_98(int *n);
+{
+	*n = 98;
+	return (0);
+}
