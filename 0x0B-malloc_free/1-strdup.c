d0762ab3bd26737926dad002bbd39eff94f70589
afristorm-3 <afristorm3@gmail.com>
2022-10-04 04:09:26 -0400

    return pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - create an array using malloc
+ * @str: size of the array
+ * Return: return a pointer
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	unsigned int cont, f;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	else
+	{
+		cont = 0;
+		while (*(str + cont) != '\0')
+		{
+			cont++;
+		}
+		copy = malloc(cont + 1);
+		if (!copy)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (f = 0; f <= cont; f++)
+			{
+				copy[f] = str[f];
+			}
+			return (copy);
+		}
+	}
+}
