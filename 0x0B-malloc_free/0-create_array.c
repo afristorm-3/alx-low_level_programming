d344c0c9d7d0e226d250c73bc0d8ed5444205e00
afristorm-3 <afristorm3@gmail.com>
2022-10-04 04:06:15 -0400

    array of character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - pasing values to other array
+ * @size: Size of the array
+ * @c: Variable to initialize the array
+ * Return: arr, the array they need
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(size * sizeof(char));
+
+	if (arr == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+
+	return (arr);
+}
