072189f998dc142695f880b38a480f5fc848cd36
afristorm-3 <afristorm3@gmail.com>
2022-10-10 14:52:31 -0400

    struct first

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+0- Define a new type struct dog with the following elements: name, type = char * age, type = float owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - create the struction of a dog
+ * @name: First element or member
+ * @age: Second member
+ * @owner: third member
+ * Description: main members of a dog
+ */
+
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+#endif
