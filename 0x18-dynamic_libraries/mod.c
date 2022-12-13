d22e0bd68d44f8bee441d700e83245dc9d6d51ff
afristorm-3 <afristorm3@gmail.com>
2022-12-13 02:37:51 -0500

    modular

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mod- the remainder of two integers
+ * @a: int is divided by b
+ * @b: int is dividing a
+ * Return: the result of the operation
+ */
+
+
+int mod(int a, int b)
+
+{
+	return (a % b);
+}
